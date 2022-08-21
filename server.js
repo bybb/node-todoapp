0181c622acfb068f2e7ab3ef96b5eff1d0eb7864
oned-main <ai9gle@gmail.com>
2022-08-22 01:03:16 +0900

    /add 추가

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
     console.log('Listening on 8080');
 });
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function(req, res){
+    console.log(req.body);
+});
