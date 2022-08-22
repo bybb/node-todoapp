a3891efa39b5d2009276d0618ac3a7d989e1f30f
oned-main <ai9gle@gmail.com>
2022-08-23 00:35:28 +0900

    list.ejs add

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
-app.listen(8080, function(){
-    console.log('Listening on 8080');
+var db;
+MongoClient.connect('mongodb+srv://ai9gle:4fkdgo@cluster0.b5db193.mongodb.net/todoapp?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client){
+    if (err) return console.log(err);
+    db = client.db('todoapp');
+
+    app.listen(8080, function(){
+        console.log('listening 8080');
+    });
 });
 
 app.get('/', function(req, res){
@@ -16,5 +24,11 @@ app.get('/write', function(req, res){
 });
 
 app.post('/add', function(req, res){
-    console.log(req.body);
+    db.collection('post').insertOne({ title: req.body.title, date: req.body.date }, function(err, result){
+        console.log('저장완료');
+    });
+});
+
+app.get('/list', function(req, res){
+    res.render('list.ejs');
 });
