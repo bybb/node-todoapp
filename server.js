ad3460f37f517a7428d653983637ce16e8a9362d
oned-main <ai9gle@gmail.com>
2022-09-17 15:52:14 +0900

    part2 end

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
 var db;
 MongoClient.connect('mongodb+srv://ai9gle:4fkdgo@cluster0.b5db193.mongodb.net/todoapp?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client){
@@ -16,11 +17,11 @@ MongoClient.connect('mongodb+srv://ai9gle:4fkdgo@cluster0.b5db193.mongodb.net/to
 });
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index.ejs');
 });
 
 app.get('/write', function(req, res){
-    res.sendFile(__dirname + '/write.html');
+    res.render('write.ejs');
 });
 
 app.post('/add', function(req, res){
@@ -38,5 +39,20 @@ app.post('/add', function(req, res){
 app.get('/list', function(req, res){
     db.collection('post').find().toArray(function(err, result){
         res.render('list.ejs', { posts : result });
+    });
+});
+
+app.delete('/delete', function(req, res){
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, function(err, result){
+        console.log('삭제완료');
+        res.status(200).send({ message : '성공했습니다' });
+    });
+});
+
+app.get('/detail/:id', function(req, res){
+    db.collection('post').findOne({ _id : parseInt(req.params.id) }, function(err, result){
+        console.log(result);
+        res.render('detail.ejs', { data : result });
     });
 });
