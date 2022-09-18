9ef92533dcf5ee069fde72bab8a8e3525d66c284
oned-main <ai9gle@gmail.com>
2022-09-19 02:20:13 +0900

    login add

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
@@ -67,8 +74,56 @@ app.get('/edit/:id', function(req, res){
     });
 });
 
-app.put('/edit/:id', function(req, res){
+app.put('/edit', function(req, res){
     db.collection('post').updateOne({ _id : parseInt(req.body.id) }, { $set : { title : req.body.title, date : req.body.date }}, function(err, result){
         console.log('수정완료');
+        res.redirect('/list');
+    });
+});
+
+app.get('/login', function(req, res){
+    res.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(req, res){
+    res.redirect('/');
+});
+
+app.get('/mypage', isLogin, function(req, res){
+    res.render('mypage.ejs', { user : req.user });
+});
+
+function isLogin(req, res, next){
+    if (req.user) {
+        next();
+    } else {
+        res.send('로그인안하셨는데요?');
+    }
+}
+
+passport.use(new LocalStrategy({
+    usernameField : 'id',
+    passwordField : 'pw',
+    session : true,
+    passReqToCallback : false
+}, function (id, pw, done) {
+    db.collection('login').findOne({id : id}, function (err, result) {
+        if (err) return done(err);
+        if (!result) return done(null, false, {message : '존재하지 않는 아이딥니다.'});
+        if (pw == result.pw) {
+            return done(null, result);
+        } else {
+            return done(null, false, {message : '비번틀렸어요'});
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    db.collection('login').findOne({id : id}, function(err, result){
+        done(null, result);
     });
 });
